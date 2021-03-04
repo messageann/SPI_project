7387c7806d3b6ffd6fdf32237646ada848a5e2e5
Ann K <no_matter>
2021-03-05 02:41:45 +0300

    beginning of folder info and data service

---
+++ Tests/DataModule/DataModule/DataService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Core3Tests.DataModule
+{
+	public class DataService
+	{
+		#region app extra data consts
+		private const byte _bytesPerExtradata = 100;
+
+
+		private const byte _bytesPerLastLoginfoId = 2;//uint16
+		private const byte _bytesPerLastFolderinfoId = 2;//uint16
+		private const byte _bytesPerFoldersCount = 2;//uint16
+		private const byte _bytesPerEmptyFoldersCount = 2;//uint16
+
+		private const int DEFAULT_FOLDERS_COUNT = 16;
+		#endregion app extra data consts
+
+		#region fields
+		private UInt16 _lastLoginfoId;
+		private UInt16 _lastFolderinfoId;
+		private UInt16 _foldersCount;
+		private UInt16 _emptyFoldersCount;
+		private readonly string _dataPath;
+		private readonly Queue<long> _emptyFolderPoses;
+
+		private readonly List<FolderInfo> _folders;
+		#endregion fields
+
+
+		public DataService(string dataPath)
+		{
+			_dataPath = dataPath;
+			_emptyFolderPoses = new Queue<long>(DEFAULT_FOLDERS_COUNT);
+			_folders = new List<FolderInfo>(DEFAULT_FOLDERS_COUNT);
+		}
+		public void Init()
+		{
+			if (!File.Exists(_dataPath))
+			{
+				using (var fs = new FileStream(_dataPath, FileMode.CreateNew, FileAccess.Write, FileShare.None, 1, FileOptions.SequentialScan))
+				{
+					fs.SetLength(_bytesPerExtradata);
+				}
+				return;
+			}
+			using (var fs = new FileStream(_dataPath, FileMode.Open, FileAccess.Read, FileShare.None, FolderInfo.BYTES_BODY, FileOptions.SequentialScan))
+			using (BinaryReader br = new BinaryReader(fs))
+			{
+				_lastLoginfoId = br.ReadUInt16();
+				_lastFolderinfoId = br.ReadUInt16();
+				_foldersCount = br.ReadUInt16();
+				_emptyFoldersCount = br.ReadUInt16();
+				fs.Seek(_bytesPerExtradata, SeekOrigin.Begin);
+				long preFolderPos;
+				UInt32 status;
+				byte[] buffer = new byte[FolderInfo.BYTES_BODY];
+				_folders.Capacity = _foldersCount.ToTheUpperPowerOf2();
+				while (_emptyFolderPoses.Count < _emptyFoldersCount)
+				{
+					preFolderPos = fs.Position;
+					status = br.ReadUInt32();
+					if ((status & (uint)StatusEnum.NULL) == 1)//null folder
+					{
+						_emptyFolderPoses.Enqueue(preFolderPos);
+						fs.Seek(preFolderPos + FolderInfo.BYTES_BODY + FolderInfo.DEFAULT_QUANTITY * LogInfo.EmptyLogInfo.Length, SeekOrigin.Begin);
+					}
+					else//non null folder
+					{
+						fs.Seek(preFolderPos, SeekOrigin.Begin);
+						_folders.Add(FolderInfo.ReadBodyFromFile(br));
+					}
+				}
+				while (_folders.Count < _foldersCount)
+				{
+					preFolderPos = fs.Position;
+					_folders.Add(FolderInfo.ReadBodyFromFile(br));
+				}
+			}
+		}
+
+		public void AddNewFolder(StatusEnum status, string name, string descr)
+		{
+			using (var fs = new FileStream(_dataPath, FileMode.Open, FileAccess.Write, FileShare.None, FolderInfo.BYTES_BODY, FileOptions.SequentialScan))
+			using (BinaryWriter bw = new BinaryWriter(fs))
+			{
+				fs.Seek(_bytesPerLastLoginfoId, SeekOrigin.Begin);
+				bw.Write(++_lastFolderinfoId);
+				bw.Write(++_foldersCount);
+				FolderInfo res;
+				if (_emptyFolderPoses.Count > 0 && status == StatusEnum.Normal)//if open space exists
+				{
+					fs.Seek(_emptyFolderPoses.Dequeue(), SeekOrigin.Begin);
+					res = new FolderInfo(fs.Position, status, 0, _lastFolderinfoId, name, descr);
+					res.WriteBodyToFile(bw);
+					for (int i = 0; i < FolderInfo.DEFAULT_QUANTITY; i++)
+					{
+						fs.Write(LogInfo.EmptyLogInfo);
+					}
+				}
+				else//to EOF
+				{
+					fs.Seek(fs.Length, SeekOrigin.Begin);
+					res = new FolderInfo(fs.Position, status, 0, _lastFolderinfoId, name, descr);
+					res.WriteBodyToFile(bw);
+				}
+				_folders.Add(res);
+			}
+		}
+
+		//public bool RemoveFolder(UInt16 id)
+		//{
+		//	for (int i = 0; i < _folders.Count; i++)
+		//	{
+		//		if (_folders[i].Id == id)
+		//		{
+		//			return true;
+		//		}
+		//	}
+		//	return false;
+		//}
+
+		public ReadOnlyCollection<FolderInfo> Folders => _folders.AsReadOnly();
+	}
+}
---
+++ Tests/DataModule/DataModule/FolderInfo.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Core3Tests.DataModule
+{
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Pack = 1)]
+	public class FolderInfo
+	{
+		#region CONSTS
+		internal const int DEFAULT_QUANTITY = 64;
+
+		internal const int BYTES_STATUS = 4;
+		internal const int BYTES_QUANTITY = 2;
+		private const int BYTES_ID = 2;
+		private const int BYTES_NAME = 40;//dividable by 8
+		private const int BYTES_DESCR = 144;//dividable by 8
+		internal const int BYTES_BODY = 0
+			+ BYTES_STATUS
+			+ BYTES_QUANTITY
+			+ BYTES_ID
+			+ BYTES_NAME
+			+ BYTES_DESCR
+			;
+		#endregion //CONSTS
+
+		#region BODY FIELDS
+		internal readonly StatusEnum Status;
+
+		internal UInt16 Quantity;
+
+		internal readonly UInt16 Id;
+
+		private string Name;
+
+		private string Descr;
+		#endregion //BODY FIELDS
+
+		internal readonly List<LogInfo> _logInfos;
+
+		internal readonly long FilePos;
+
+		public FolderInfo(long filePos, StatusEnum status, ushort quantity, ushort id, string name, string descr)
+		{
+			Status = status;
+			Id = id;
+			Name = name;
+			Descr = descr;
+			Quantity = quantity;
+			_logInfos = new List<LogInfo>((int)status / 2 * DEFAULT_QUANTITY);
+			FilePos = filePos;
+		}
+
+		public void WriteBodyToFile(BinaryWriter bw)
+		{
+			bw.Write((UInt32)Status);
+			bw.Write(Quantity);
+			bw.Write(Id);
+			byte[] nameb = new byte[BYTES_NAME];
+			Encoding.Default.GetBytes(Name, 0, Name.Length.EqualOrLess(BYTES_NAME*2), nameb, 0);
+			bw.Write(nameb);
+
+			byte[] descrb = new byte[BYTES_DESCR];
+			Encoding.Default.GetBytes(Descr, 0, Descr.Length.EqualOrLess(BYTES_DESCR*2), descrb, 0);
+			bw.Write(descrb);
+			for (int i = 0; i < _logInfos.Capacity; i++)
+			{
+				bw.Write(LogInfo.EmptyLogInfo);
+			}
+			var nb = GetNullBytes();
+			if (nb > 0)
+			{
+				byte[] nullb = new byte[nb];
+				bw.Write(nullb);
+			}
+		}
+
+		public static FolderInfo ReadBodyFromFile(BinaryReader br)
+		{
+			var res = new FolderInfo(br.BaseStream.Position, (StatusEnum)br.ReadUInt32(), br.ReadUInt16(), br.ReadUInt16(),
+				Encoding.Default.GetString(br.ReadBytes(BYTES_NAME)), Encoding.Default.GetString(br.ReadBytes(BYTES_DESCR)));
+			br.BaseStream.Seek(br.BaseStream.Position + res._logInfos.Capacity * LogInfo.BYTES_LOGINFO + res.GetNullBytes(), SeekOrigin.Begin);
+			return res;
+		}
+
+		private static readonly byte[] NullBody;
+
+		static FolderInfo()
+		{
+			NullBody = new byte[BYTES_BODY];
+			NullBody[0] = 1;
+		}
+	}
+
+	public enum StatusEnum : UInt32
+	{
+		NULL = 1U << 0,
+		Normal = 1U << 1,
+		X2 = 1U << 2,
+		X4 = 1U << 3,
+		X8 = 1U << 4
+	}
+}
