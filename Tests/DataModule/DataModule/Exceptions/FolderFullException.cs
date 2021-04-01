a8da27444a0ee0268f7c89e5301dc57592cfb627
qq bb <qq2bb@mail.ru>
2021-04-02 01:53:47 +0300

    Added exceptions

---
+++ Tests/DataModule/DataModule/Exceptions/FolderFullException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataModule.Exceptions
+{
+	[Serializable]
+	public class FolderFullException : Exception
+	{
+		public FolderFullException(FolderInfo fi) : base($"Folderinfo[id:{fi.Id}] is full!") { }
+		protected FolderFullException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
---
+++ Tests/DataModule/DataModule/Exceptions/FolderNotCachedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataModule.Exceptions
+{
+	[Serializable]
+	public class FolderNotCachedException : Exception
+	{
+		public FolderNotCachedException() { }
+		public FolderNotCachedException(FolderInfo fi) : base($"Folderinfo[id:{fi.Id}] not cached!") { }
+		protected FolderNotCachedException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
