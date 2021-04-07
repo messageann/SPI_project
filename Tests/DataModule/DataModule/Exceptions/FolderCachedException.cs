78524c1c9406bc0e969aa131f253b45bd0bc3a4b
ktoyam <nikita.shatokhin_a@mail.ru>
2021-04-07 17:45:33 +0300

    added exception

---
+++ Tests/DataModule/DataModule/Exceptions/FolderCachedException.cs
@@ -0,0 +1,16 @@
+﻿using DataModule.Models;
+using System;
+
+namespace DataModule.Exceptions
+{
+
+	[Serializable]
+	public class FolderCachedException : Exception
+	{
+		public FolderCachedException() { }
+		public FolderCachedException(FolderInfo fi) : base($"Folderinfo[id:{fi.Id}] already cached!") { }
+		protected FolderCachedException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
