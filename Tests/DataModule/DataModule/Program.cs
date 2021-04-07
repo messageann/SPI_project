9f9566d6c01f2ae8d4e02d06ea472e56602ab3b8
ktoyam <nikita.shatokhin_a@mail.ru>
2021-04-07 19:33:59 +0300

    tests update

--- Tests/DataModule/DataModule/Program.cs
+++ Tests/DataModule/DataModule/Program.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using DataModule.Models;
+using System;
 using System.IO;
 
 namespace DataModule
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string p = @"D:\TestCore_testfolder\dataENC";
-            if (File.Exists(p)) File.Delete(p);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string p = @"D:\TestCore_testfolder\dataENC";
+			if (File.Exists(p)) File.Delete(p);
+			using (var ds = new DataService(p))
 			{
-                DataService ds = new DataService(p);
-                ds.Init();
-                //ds.RegLogInfo(new Models.FolderInfo())
-			}            
-        }
-    }
+				ds.Init();
+				ds.RegFolderInfo(StatusEnum.Normal, "folder one", "normal length folder 1st");
+			}
+			using (var ds = new DataService(p))
+			{
+				ds.Init();
+				ds.RegLogInfo(ds.Folders[0], "google acc1", "old account, almost empty", "myaccount_old@gmail.com", "qwe123rty", new CryptMethod("key1"));
+			}
+			using (var ds = new DataService(p))
+			{
+				ds.Init();
+				ds.ReadFolderInfoContent(ds.Folders[0]);
+			}
+		}
+	}
 }
