ca51da89aca3cc47bab2f87b4e2a947aff58acce
хм да ето кисулькен <night.jesus.of.suburbia@yandex.ru>
2021-04-15 15:45:15 +0300

    windowchrome, preimplementation of adding new folder

--- Encoder/UI/ViewModels/EncoderWindowVM.cs
+++ Encoder/UI/ViewModels/EncoderWindowVM.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using DataModule;
+using DataModule.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 using WPFCoreEx.Bases;
 
 namespace UI.ViewModels
 {
 	public class EncoderWindowVM : INotifyPropertyChanged
 	{
+		private readonly DataService _ds;
+
+		public EncoderWindowVM(DataService ds)
+		{
+			_ds = ds;
+		}
+
 		public string Title => "Encoder";
+		public ReadOnlyObservableCollection<FolderInfo> Folders => _ds.Folders;
+
+		private RelayCommand _preAddFolderInfo;
+		public ICommand PreAddFolderInfo
+		{
+			get
+			{
+				if (_preAddFolderInfo == null) _preAddFolderInfo = new RelayCommand((o) => _ds.PreaddFolderInfo());
+				return _preAddFolderInfo;
+			}
+		}
 
 		#region NOTIFS
 		public event PropertyChangedEventHandler PropertyChanged;
