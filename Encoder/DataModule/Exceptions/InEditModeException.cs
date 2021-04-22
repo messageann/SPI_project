822ee174e7473d5b84ad9ac47116c4d47dd4cd11
ktoyam <nikita.shatokhin_a@mail.ru>
2021-04-23 02:33:22 +0300

    more general exceptions

---
+++ Encoder/DataModule/Exceptions/InEditModeException.cs
@@ -0,0 +1,11 @@
+﻿using DataModule.Models;
+using System;
+
+namespace DataModule.Exceptions
+{
+	public class InEditModeException : Exception
+	{
+		internal InEditModeException(FolderInfo fi) : base($"Folderinfo[id:{fi.Id}] is in edit mode!") { }
+		internal InEditModeException(LogInfo li) : base($"Loginfo[id:{li.ID}] is in edit mode!") { }
+	}
+}
---
+++ Encoder/DataModule/Exceptions/NotUnlockedException.cs
@@ -0,0 +1,11 @@
+﻿using DataModule.Models;
+using System;
+
+namespace DataModule.Exceptions
+{
+	public class NotUnlockedException : Exception
+	{
+		internal NotUnlockedException(FolderInfo fi) : base($"FolderInfo[id:{fi.Id}] not unlocked!") { }
+		internal NotUnlockedException(LogInfo li) : base($"FolderInfo[id:{li.ID}] not unlocked!") { }
+	}
+}
