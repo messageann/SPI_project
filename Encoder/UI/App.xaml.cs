e3d46d641ce14ceb8dbdd601fa8b67ab480a22c6
хм да ето кисулькен <night.jesus.of.suburbia@yandex.ru>
2021-04-23 03:22:54 +0300

    auth-loop

--- Encoder/UI/App.xaml.cs
+++ Encoder/UI/App.xaml.cs
@@ -6,6 +6,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using DataModule;
+using UI.ViewModels;
 
 namespace UI
 {
@@ -14,22 +17,46 @@ namespace UI
 	/// </summary>
 	public partial class App : Application
 	{
+		private bool _isShutdowned = false;
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			var initres = ViewModels.ViewModelsController.Init();
-			if (initres)
+			ViewModelsController.Init();
+			while (!_isShutdowned)
 			{
-				new EncoderWindow().Show();
-			}
-			else
-			{
-				Application.Current.Shutdown(-1);
+				using (var scope = ViewModelsController.GetScope())
+				{
+					var scopeProvider = scope.ServiceProvider;
+					var auth = new Authorization();
+					auth.ShowDialog();
+					DataService ds = scopeProvider.GetRequiredService<DataService>();
+					if (auth.file == null)
+					{
+						this.Shutdown(3);
+						return;
+					}
+					else
+					{
+						var res = ds.Init(auth.file, auth.password);
+						if (!res)
+						{
+							MessageBox.Show("Bad password!");
+							continue;
+						}
+						else
+						{
+							var vm = scopeProvider.GetRequiredService<EncoderWindowVM>();
+							vm.AccountName = new string(auth.file.Name.Take(auth.file.Name.LastIndexOf(auth.file.Extension)).ToArray());
+							new EncoderWindow(vm).ShowDialog();
+						}
+					}
+				}
 			}
 		}
 
 		private void Application_Exit(object sender, ExitEventArgs e)
 		{
-			ViewModels.ViewModelsController.Unload();
+			_isShutdowned = true;
+			ViewModelsController.Unload();
 		}
 	}
 }
